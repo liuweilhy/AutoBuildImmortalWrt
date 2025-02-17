295d60a486b5e57debfc4e44a2bf3ea5dab8c817
liuweilhy <liuweilhy@gmail.com>
2025-02-17 12:48:58 +0800

    修复接口配置错误

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -4,7 +4,12 @@
 LOGFILE="/tmp/uci-defaults-log.txt"
 echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
-uci set firewall.@zone[1].input='ACCEPT'
+zone_count=$(uci show firewall | grep -c "zone")
+if [ "$zone_count" -ge 2 ]; then
+   uci set firewall.@zone[1].input='ACCEPT'
+else
+   echo "Second firewall zone not found, skipping input rule setting." >> $LOGFILE
+fi
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
 uci add dhcp domain
@@ -14,13 +19,17 @@ uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # 计算网卡数量
 count=0
+ifnames=""
 for iface in /sys/class/net/*; do
   iface_name=$(basename "$iface")
   # 检查是否为物理网卡（排除回环设备和无线设备）
   if [ -e "$iface/device" ] && echo "$iface_name" | grep -Eq '^eth|^en'; then
     count=$((count + 1))
+    ifnames="$ifnames $iface_name"
   fi
 done
+# 删除多余空格
+ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 
 # 检查配置文件pppoe-settings是否存在 该文件由build.sh动态生成
 SETTINGS_FILE="/etc/config/pppoe-settings"
@@ -37,32 +46,49 @@ if [ "$count" -eq 1 ]; then
    # 单网口设备 不支持修改ip 不要在此处修改ip 
    uci set network.lan.proto='dhcp'
 elif [ "$count" -gt 1 ]; then
-   # 获取所有LAN物理接口
-   lan_ifnames=$(uci get network.lan.ifname)
    # 提取第一个接口作为WAN
-   wan_iface=$(echo "$lan_ifnames" | awk '{print $1}')
+   wan_ifname=$(echo "$ifnames" | awk '{print $1}')
    # 剩余接口保留给LAN
-   new_lan_ifnames=$(echo "$lan_ifnames" | cut -d ' ' -f2-)
+   lan_ifnames=$(echo "$ifnames" | cut -d ' ' -f2-)
    # 设置WAN接口基础配置
    uci set network.wan=interface
-   uci set network.wan.ifname="$wan_iface"
+   # 提取第一个接口作为WAN
+   uci set network.wan.device="$wan_ifname"
    # WAN接口默认DHCP
    uci set network.wan.proto='dhcp'
+   # 设置WAN6接口基础配置
+   uci set network.wan6=interface
+   uci set network.wan6.device='@wan'
    # 更新LAN接口成员
-   uci set network.lan.ifname="$new_lan_ifnames"
+   # 查找对应设备的section名称
+   section=$(uci show network | awk -F '[.=]' '/\.@?device\[\d+\]\.name=.br-lan.$/ {print $2; exit}')
+   if [ -z "$section" ]; then
+      echo "error：cannot find device 'br-lan'." >> $LOGFILE
+   else
+      # 删除所有现有的ports列表
+      uci -q delete "network.$section.ports"
+      # 添加新的ports列表（从数组的第二个元素开始）
+      for port in $lan_ifnames; do
+         uci add_list "network.$section.ports"="$port"
+      done
+      echo "ports of device 'br-lan' are update." >> $LOGFILE
+   fi
+   # LAN口设置静态IP
+   uci set network.lan.proto='static'
    # 多网口设备 支持修改为别的ip地址
    uci set network.lan.ipaddr='192.168.123.1'
+   uci set network.lan.netmask='255.255.255.0'
    echo "set 192.168.123.1 at $(date)" >> $LOGFILE
    # 判断是否启用 PPPoE
    echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    if [ "$enable_pppoe" = "yes" ]; then
       echo "PPPoE is enabled at $(date)" >> $LOGFILE
       # 设置宽带拨号信息
-      uci set network.wan.proto='pppoe'                
-      uci set network.wan.username=$pppoe_account     
-      uci set network.wan.password=$pppoe_password     
-      uci set network.wan.peerdns='1'                  
-      uci set network.wan.auto='1' 
+      uci set network.wan.proto='pppoe'
+      uci set network.wan.username=$pppoe_account
+      uci set network.wan.password=$pppoe_password
+      uci set network.wan.peerdns='1'
+      uci set network.wan.auto='1'
       echo "PPPoE configuration completed successfully." >> $LOGFILE
    else
       echo "PPPoE is not enabled. Skipping configuration." >> $LOGFILE
@@ -78,7 +104,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by liuweilhy"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
